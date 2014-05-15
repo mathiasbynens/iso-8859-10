38024dd26a70abca1f08cb27f55f7104c6a6e828
Mathias Bynens <mathias@qiwi.be>
2014-05-15 15:52:08 +0200

    Tag the v0.1.2 release

--- iso-8859-10.js
+++ iso-8859-10.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/iso-8859-10 v0.1.1 by @mathias | MIT license */
+/*! http://mths.be/iso-8859-10 v0.1.2 by @mathias | MIT license */
 ;(function(root) {
 
 	// Detect free variables `exports`.
@@ -124,7 +124,7 @@
 			'l6',
 			'latin6'
 		],
-		'version': '0.1.1'
+		'version': '0.1.2'
 	};
 
 	// Some AMD build optimizers, like r.js, check for specific condition patterns
