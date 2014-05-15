00bf612167db8f5febd02d7bf02a8caa3dbbd5fa
Mathias Bynens <mathias@qiwi.be>
2014-05-15 15:50:21 +0200

    package.json: Add `legacy-encoding` keyword

--- tests/tests.src.js
+++ tests/tests.src.js
@@ -35,7 +35,7 @@
 
 	// explicitly call `QUnit.module()` instead of `module()`
 	// in case we are in a CLI environment
-	QUnit.module('iso885910');
+	QUnit.module('iso-8859-10');
 
 	test('iso885910.encode', function() {
 		equal(
